92edfad8f544a6dbd51e09bf0beed6041ddc0da5
carloshamiltonvasconcelos <88862292+carloshamiltonvasconcelos@users.noreply.github.com>
2025-07-16 18:04:55 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Iolanda, como é grande o meu amor por você e se você me amar sorria.</h1>
+    return <h1>Iolanda, como é grande o meu amor por você e se você me ama sorria.</h1>
 }
 
 export default Home;
