9bea54046f4b79364023db795154da9423fb8ae5
carloshamiltonvasconcelos <88862292+carloshamiltonvasconcelos@users.noreply.github.com>
2025-07-17 15:53:27 +0000

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Iolanda, o meu amor por você é tão grande que eu não consigo nem dizer o quanto te amo!</h1>
+  return (
+    <h1>
+      Iolanda, o meu amor por você é tão grande que eu não consigo nem dizer o
+      quanto te amo!
+    </h1>
+  );
 }
 
 export default Home;
